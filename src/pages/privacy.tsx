19eb668055389ab20a4cde0d16dacdfaee99bcdc
milan aryal <milanaryal74@gmail.com>
2023-07-05 19:22:21 +0545

    privacy policy has been updated

--- src/pages/privacy.tsx
+++ src/pages/privacy.tsx
@@ -24,12 +24,12 @@ export default function PrivacyPage() {
         path="privacy"
       />
       <PageHeroSection heroTitle="text-page-privacy-policy" />
-      <div className="py-12 lg:py-16 2xl:py-20 xl:px-16 2xl:px-24 3xl:px-36">
+      <div className=" md:py-4  xl:px-16 2xl:px-24 3xl:px-36 " >
         <Container>
-          <div className="flex flex-col md:flex-row">
-            <nav className="hidden sm:block md:w-72 xl:w-3/12 mb-8 2xl:mb-0 lg:-mt-2">
+          <div className="flex flex-col md:flex-row ">
+            <nav className="hidden sm:block md:w-72 xl:w-3/12 mb-8 2xl:mb-0 lg:-mt-2 ">
               <ol className="sticky md:top-16 lg:top-20 z-10">
-                {privacyPolicy?.map((item, index) => (
+                {privacyPolicy?.privacy?.map((item, index) => (
                   <li key={index}>
                     <Link
                       spy={true}
@@ -48,23 +48,52 @@ export default function PrivacyPage() {
             </nav>
             {/* End of section scroll spy menu */}
 
-            <div className="md:w-9/12 md:ps-8">
-              {privacyPolicy?.map((item) => (
+            <div className="md:w-9/12 md:ps-8 ">
+              {
+                <p className="text-md md:text-lg font-md text-justify text-black ">{t(privacyPolicy.main_description)}</p>
+              }
+              {privacyPolicy?.privacy?.map((item) => (
                 // @ts-ignore
                 <Element
-                  key={item.title}
+                  key={item.id}
                   id={makeTitleToDOMId(t(item.title))}
-                  className="mb-8 lg:mb-12 last:mb-0 order-list-enable"
+                  className="mb-8 my-4 lg:my-12 last:mb-0 order-list-enable "
                 >
-                  <Heading className="mb-4 lg:mb-6 font-body" variant="title">
-                    {t(item.title)}
+                  <Heading className="mb-4 lg:mb-6 font-body text-dark-footer  " variant="title">
+                  <span className=" mr-1 md:mr-3">{item.id}.</span>  {t(item.title)}
                   </Heading>
                   <div
-                    className="text-skin-muted text-sm lg:text-15px leading-7 space-y-5"
+                    className="text-justify text-black text-md lg:text-15px leading-6 md:leading-7  "
                     dangerouslySetInnerHTML={{
-                      __html: t(item.description),
+                      __html: item?.sub_description
                     }}
                   />
+                  {
+                    item.points&&item.points.map((sub_item,index)=>{
+                      return(
+                        <div key={sub_item?.point_id} >
+                        <h3  className="text-md md:text-md xl:text-md my-4 md:my-6 text-black font-semibold">
+                        <span className=" mx-1 md:mx-2 ">{sub_item?.point_id}.</span>  {t(sub_item?.point_title)}
+                        </h3>
+                        <div
+                        className="text-black text-md md:text-md leading-7 space-y-5 text-justify"
+                        key={sub_item?.point_id}
+                        dangerouslySetInnerHTML={{
+                          __html: sub_item?.point_description
+                        }}
+                      />
+                      {
+                        sub_item?.sub_points&&sub_item?.sub_points?.map((sub_point,index)=>{
+                          return(
+                            <p key={index} className="text-md text-black leading-7 my-4 mb-2 ml-1 md:ml-3"><span className="text-xl font-bold text-black mr-2">.</span>{sub_point?.sub_points_description}</p>
+                          )
+                        })
+                      }
+                      </div>
+                      )
+                    })
+                  }
+             
                 </Element>
               ))}
             </div>
