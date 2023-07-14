b80323013867f2557094d8f3a837bbba43f293cb
Beenet77 <beenet.nepal123@gmail.com>
2023-07-14 21:52:52 +0545

    COnflicr resolved

--- src/components/layout-dashboard-student/index.tsx
+++ src/components/layout-dashboard-student/index.tsx
@@ -1,14 +1,8 @@
-<<<<<<< Updated upstream
-import React, { useState } from 'react';
-import Sidebar from '@components/layout-dashboard-student/sidebar';
-import Header from '@components/layout-dashboard-student/header';
-import StickyMenu from './menu';
-import DashboardHeader from './header/dashboardheader';
-=======
 import React, { useState } from "react";
 import Sidebar from "@components/layout-dashboard-student/sidebar";
 import Header from "@components/layout-dashboard-student/header";
->>>>>>> Stashed changes
+import StickyMenu from "./menu";
+import DashboardHeader from "./header/dashboardheader";
 
 export default function ProfileLayout({ children }: React.PropsWithChildren) {
   const [sidebarOpen, setSidebarOpen] = useState<boolean>(true);
@@ -19,13 +13,13 @@ export default function ProfileLayout({ children }: React.PropsWithChildren) {
 
       <div className="relative flex flex-col flex-1 overflow-y-auto overflow-x-hidden h-[2200px] border border-solid border-black">
         {/* <Header sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} /> */}
-       
-          <DashboardHeader/> {/* new header of student dashboard for mobile and tablet*/}
+        <DashboardHeader />{" "}
+        {/* new header of student dashboard for mobile and tablet*/}
         <main>
           <div className="px-4 py-2 w-full max-w-9xl mx-auto">{children}</div>
         </main>
       </div>
-      <StickyMenu/>
+      <StickyMenu />
     </div>
   );
 }
