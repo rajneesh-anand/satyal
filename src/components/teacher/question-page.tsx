793c5eaae11f79039afee6296700ffa2d199d695
Beenet77 <beenet.nepal123@gmail.com>
2023-07-18 17:53:17 +0545

    changes in the question paper page

--- src/components/teacher/question-page.tsx
+++ src/components/teacher/question-page.tsx
@@ -6,8 +6,26 @@ function Questions() {
   const [subjects, setSubjects] = useState([]);
   const [selectedSubject, setSelectedSubject] = useState();
   const [className, setClassName] = useState();
+  const [selectedQuestions, setSelectedQuestions] = useState([]);
 
   const [data, setData] = useState([]);
+  // console.log(selectedQuestions);
+
+  function handleQuestionChange(value) {
+    const a = selectedQuestions.some(
+      (item) => JSON.stringify(item) === JSON.stringify(value)
+    );
+    console.log(a);
+    if (Boolean(a)) {
+      const result = selectedQuestions.filter(
+        (el) => value.Image_Minio_Link !== el.Image_Minio_Link
+      );
+      console.log(result);
+      setSelectedQuestions(result);
+    } else {
+      setSelectedQuestions([...selectedQuestions, value]);
+    }
+  }
 
   function handleClassChange(value) {
     setClassName(value.value);
@@ -27,7 +45,6 @@ function Questions() {
 
     fetch;
     const { questions } = await res.json();
-    console.log(questions);
     setData(questions);
   }
 
@@ -35,7 +52,6 @@ function Questions() {
     selectedSubject && getQuestions();
   }, [selectedSubject]);
 
-  console.log(data);
   return (
     <div>
       <div className="flex gap-2">
@@ -51,23 +67,60 @@ function Questions() {
           onChange={handleSubjectChange}
         />
       </div>
-      <div className="flex">
-        <ul className="border border-2 mt-1 w-2/5">
-          {data?.map((el) => (
-            <li className=" border-y-2 m-2">
-              <input type="checkbox" className="mr-1 mb-1" />
-              <span>{el.Question}</span>
 
-              <img
-                src={el.Image_Minio_Link}
-                className="w-34 object-cover"
-                // alt="Book cover"
-              />
-            </li>
-          ))}
-        </ul>
-        <div className="w-2/3 border border-2 mt-1 ml-1">asdfghj</div>
-      </div>
+      {data.length ? (
+        <div className="flex">
+          <ul className="border border-2 mt-1 w-2/5">
+            {data.map((el) => (
+              <li className=" border-b-2 m-2">
+                <input
+                  type="checkbox"
+                  className="mr-1 mb-1"
+                  onChange={() => handleQuestionChange(el)}
+                />
+                <span>{el.Question}</span>
+
+                <img
+                  src={el.Image_Minio_Link}
+                  className="w-34 object-cover"
+                  alt={el.Image_Link}
+                />
+              </li>
+            ))}
+          </ul>
+          <div className="w-2/3 border border-2 mt-1 ml-1">
+            {selectedQuestions.length ? (
+              <div>
+                <ul>
+                  {selectedQuestions.map(
+                    (el) =>
+                      el && (
+                        <li className=" border-b-2 m-2">
+                          <span>{el.Question}</span>
+
+                          <img
+                            src={el.Image_Minio_Link}
+                            className="w-34 object-cover"
+                            alt={el.Image_Link}
+                          />
+                        </li>
+                      )
+                  )}
+                </ul>
+              </div>
+            ) : (
+              <div>
+                <h3>Select Questions to view them here.</h3>
+              </div>
+            )}
+          </div>
+        </div>
+      ) : (
+        <div className=" flex justify-center mt-16">
+          {/* // <h3 className="text-xl md:text-2xl font-semibold text-dark-footer "> */}
+          <h3>Sorry! Data are not available</h3>
+        </div>
+      )}
     </div>
   );
 }
