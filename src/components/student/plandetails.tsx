12b23479e2276d03743e4e9c9414cf86bcc31076
milan aryal <milanaryal74@gmail.com>
2023-08-28 14:44:48 +0545

    profile page is updated for both teacher and student

---
+++ src/components/student/plandetails.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function plandetails() {
+  return (
+    <>
+    <div className='w-full my-[20px] sm:my-[40px]'>
+        <div className='w-full mx-auto lg:w-5/6 py-4 px-4 sm:px-[40px] bg-secondary-background rounded-md'>
+        <h1>Student's plan details</h1>
+        </div>
+     
+      </div>
+      
+    </>
+  )
+}
