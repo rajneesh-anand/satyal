aced2f2172f2b1a0dd1689650fa6943d3aeee647
Beenet77 <beenet.nepal123@gmail.com>
2023-10-02 15:31:41 +0545

    filter feature updated with link

--- src/pages/teacher/onlineclass.tsx
+++ src/pages/teacher/onlineclass.tsx
@@ -1,87 +1,74 @@
-import React, { useEffect, useState } from "react";
-import TeacherDashboardLayout from "@components/layout-dashboard-teacher";
-import Select from "@components/ui/select/select";
-import { subject_inClass,classOptions,ValueType } from "@data/constant";
-import Modal from "@components/common/modal/modal";
-import OnlineClassModal from "../../components/teacher/onlineclassmodal";
-import { ButtonSize, ButtonType } from "../../../enums/buttons";
-import { Button } from "@components/ui/button/dashboard-button";
 import Onlinecard from "@components/cards/onlinecard";
-import { serverSideTranslations } from "next-i18next/serverSideTranslations";
-import { GetServerSideProps, GetStaticProps } from "next";
-import { getSession, useSession } from "next-auth/react";
-import Loader from "@components/ui/loader/loader";
+import Modal from "@components/common/modal/modal";
+import TeacherDashboardLayout from "@components/layout-dashboard-teacher";
+import { Button } from "@components/ui/button/dashboard-button";
 import Heading from "@components/ui/heading";
-import {HeadingType} from '../../../enums/tittle'
-
+import Loader from "@components/ui/loader/loader";
+import Select from "@components/ui/select/select";
+import { subject_inClass } from "@data/constant";
+import { GetServerSideProps } from "next";
+import { getSession, useSession } from "next-auth/react";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import { ButtonSize, ButtonType } from "../../../enums/buttons";
+import { HeadingType } from "../../../enums/tittle";
+import OnlineClassModal from "../../components/teacher/onlineclassmodal";
 
 export default function onlineclass() {
-  let {data:session}=useSession()
+  let { data: session } = useSession();
   const [openModal, setOpenModal] = useState(false);
-  const [classFilter,setClassFilter]=useState('') //store class to filter createClass
-  const [apiClassList,setApiClassList]=useState([]);
-  const [filteredClassList,setFilteredClassList]=useState([]);//after feltering onlineclass by teacher
- const [responseClass,setResponseClass]=useState();
- const [loader,setLoader]=useState(true);
+  // const [classFilter, setClassFilter] = useState(""); //store class to filter createClass
+  const [classList, setClassList] = useState([]);
+  const [apiClassList, setApiClassList] = useState([]);
+
+  // const [filteredClassList, setFilteredClassList] = useState([]); //after feltering onlineclass by teacher
+  const [responseClass, setResponseClass] = useState();
+  const [loader, setLoader] = useState(false);
   // all the below state are shifted into online class model
   // const [section, setSection] = useState('');
   // const [className, setClassName] = useState();
   // const [selectedSubject, setSelectedSubject] = useState();
-  // const [subjects, setSubjects] = useState([]); 
+  // const [subjects, setSubjects] = useState([]);
 
- // handeler funtion for modal state
- let handelModalState=()=>{
-  setOpenModal((state)=>!state);
-}
-let handelClassFilter=(e)=>{ 
-  setClassFilter(e?.value)
-}
-// all handeler funtion are shifted into onlineclass modal
-//   const handleInputChange = (e) => {
-//     setSection(e.target.value);
-//   };
-// //  function for changing class value in modal
-//   function handleClassChange(value) {
-//     // console.log(value);
-    
-//     setClassName(value.value);
-//     setSubjects(value.subject);
-//   }
-//   // handeler function  for select subject in modal
-//   function handleSubjectChange(value) {
-//     setSelectedSubject(value.value);
-//   }
- 
-// api fetch created class by teacher
-  useEffect(()=>{ 
-   let fetchClass=async()=>{
-    try{
-      setLoader(true);
-      let response=await fetch(`${process.env.NEXT_PUBLIC_REST_API_ENDPOINT}/onlineClassTeacher/createdClasses/${session?.user?.email}`);
-      let data=await response.json();
-      setApiClassList([...data]);
+  console.log(classList);
+  console.log(apiClassList);
 
-    }catch(err){
-      console.log(err);   
-    }  
-   }
-   fetchClass();
-  },[session?.user?.email,responseClass])
-
-// filtering online class according to class grade
-useEffect(()=>{
-  if(!classFilter){
-    setFilteredClassList([...apiClassList]);
-    setLoader(false);
-  }else{
+  // handeler funtion for modal state
+  let handelModalState = () => {
+    setOpenModal((state) => !state);
+  };
+  let handelClassFilter = (e) => {
     setLoader(true);
-    let afterFilter=apiClassList?.filter((onlineClass)=>onlineClass?.onlineClassGrade===classFilter);
-    setFilteredClassList([...afterFilter]);
-    setLoader(false)
-  }
-},[classFilter,apiClassList])
+    let result = classList.filter((el) => el.onlineClassGrade === e?.value);
+    if (result) {
+      setLoader(false);
+      setApiClassList(result);
+    }
+  };
 
+  // console.log(result);
 
+  // api fetch created class by teacher
+  useEffect(() => {
+    let fetchClass = async () => {
+      try {
+        setLoader(true);
+
+        let response = await fetch(
+          `${process.env.NEXT_PUBLIC_REST_API_ENDPOINT}/onlineClassTeacher/createdClasses/${session?.user?.email}`
+        );
+        let data = await response.json();
+        setClassList([...data]);
+        setApiClassList([...data]);
+
+        setLoader(false);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchClass();
+  }, [session?.user?.email, responseClass]);
 
   return (
     <TeacherDashboardLayout>
@@ -106,32 +93,40 @@ useEffect(()=>{
           </div>
         </div>
         <div className="w-full py-[20px] flex flex-wrap">
-          {(loader)?(
-             <div className="w-full flex justify-center ">
-               <Loader className="text-dark-footer"/>
-             </div>
-            ):(filteredClassList.length>0)?(
-            filteredClassList&&filteredClassList.map((onlineclass)=>{
-              return(
-                  <Onlinecard key={onlineclass.id} onlineclass={onlineclass}/>
-              )
-             }) 
-            ):(
-             <div className="w-full flex sm:block justify-center sm:justify-start ">
-               <Heading variant={HeadingType.MediumHeading}>Sorry There is no Class</Heading>
-             </div>
-            )
-          } 
-         </div>
+          {loader ? (
+            <div className="w-full flex justify-center ">
+              <Loader className="text-dark-footer" />
+            </div>
+          ) : apiClassList.length > 0 ? (
+            apiClassList &&
+            apiClassList.map((onlineclass) => {
+              return (
+                <Link href={"/teacher/dashboard"}>
+                  <a>
+                    <Onlinecard
+                      key={onlineclass.id}
+                      onlineclass={onlineclass}
+                    />
+                  </a>
+                </Link>
+              );
+            })
+          ) : (
+            <div className="w-full flex sm:block justify-center sm:justify-start ">
+              <Heading variant={HeadingType.MediumHeading}>
+                Sorry There is no Class
+              </Heading>
+            </div>
+          )}
+        </div>
       </div>
 
       <Modal onClose={handelModalState} open={openModal}>
         <OnlineClassModal
-        handelModalState={handelModalState}
-        setResponseClass={setResponseClass}
+          handelModalState={handelModalState}
+          setResponseClass={setResponseClass}
         />
       </Modal>
-   
     </TeacherDashboardLayout>
   );
 }
